0eb9f30a77f8795d3facc1389ce2b5cf1fd85462
Pushpak Bhagat <70573212+pushpak-256@users.noreply.github.com>
2022-05-11 14:50:27 +0530

    Time: 2 ms (76.09%), Space: 55.6 MB (45.84%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
@@ -1,30 +1,24 @@
 class Solution {
-    public int[] sortedSquares(int[] nums) {
-      if (nums.length==1) { nums[0]=nums[0]*nums[0];return nums;}
-      int left  = 0;
-      int right = nums.length-1;
-      int res [] = new int[nums.length];
-      int index = right;
-      while (left<=right)
-      {
-         int r =Math.abs(nums[right]);
-         int l = Math.abs(nums[left]);
-        if(r>l)
-        {
-          res[index--]=r*r;
-          right--;
-        }
-        else if (l>r)
-        {
-          res[index--]=l*l;
-          left++;
-        }
-        else 
-        {
-          res[index--]=l*l;
-          left++;
-        }
-      }
-      return res;
-    }
+ 	 public static int[] sortedSquares(int[] nums) {
+		 if (nums.length==1) { nums[0]=nums[0]*nums[0];return nums;}
+	        int left = 0;
+	        int right = nums.length-1;
+	        int [] res = new int [nums.length];
+	        int i = right;
+	        while (left<=right)
+	        {
+	            if (Math.abs(nums[right])>Math.abs(nums[left]) )
+	            {
+	                res[i--]=nums[right]*nums[right];
+	                right--;
+	             }
+	            else if(Math.abs(nums[right])<Math.abs(nums[left])) 
+	            {
+	             res[i--]=nums[left]*nums[left];  
+	                 left++;
+	            } 
+	            else   {res[i--]=nums[left]*nums[left]; left++;;}  
+	        }
+	        return res;
+	 }
 }
