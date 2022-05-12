5d47744e270baaed62534aeb337f8744428f9f66
Pushpak Bhagat <70573212+pushpak-256@users.noreply.github.com>
2022-05-12 19:04:01 +0530

    Time: 3 ms (28.44%), Space: 55.3 MB (14.53%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,16 +1,16 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-      int index =0;
-        for(int i =0;i<nums.length;i++)
-        {
-          if(nums[i]!=0)
-          {
-           nums[index++]=nums[i];
-          }
-        }
-       for(int i =index;i<nums.length;i++)
+     int snowBallSize =0;
+      for(int i =0;i<nums.length;i++)
        { 
-         nums[i]=0;
+         if(nums[i]==0)
+         snowBallSize++;
+         else if(snowBallSize>0)
+         {
+           int tmp =nums[i];
+            nums[i]=0;
+           nums[i-snowBallSize]=tmp;
+         }
        }
     }
 }
