637874aa4db5f2105486021f884e190fd13fbd9b
Pushpak Bhagat <70573212+pushpak-256@users.noreply.github.com>
2022-05-15 22:04:23 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (98.15%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.java
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.java
@@ -2,7 +2,7 @@ class Solution {
     public ListNode middleNode(ListNode head) {
      ListNode turtle = head;
      ListNode rabbit = head;
-      while(rabbit!=null && rabbit.next!=null)
+      while(rabbit!=null &&rabbit.next!=null)
       {
         turtle = turtle.next;
         rabbit=rabbit.next.next;
