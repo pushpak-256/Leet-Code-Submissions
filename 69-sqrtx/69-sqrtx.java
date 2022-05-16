5b2173816bb1b9275e75c7af1fe88808267a394b
Pushpak Bhagat <70573212+pushpak-256@users.noreply.github.com>
2022-05-16 12:13:29 +0530

    Time: 2 ms (71.95%), Space: 40.8 MB (69.39%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int mySqrt(int num) {
+       //Using long is key here 
+        long left = 1;
+        long right = num;
+        int res =0;
+        while (left <= right)
+        {
+            long mid = left + (right - left) / 2;
+            long sqr = mid * mid;
+            if (sqr <= num)
+            {
+             res=(int)mid;
+             left=mid+1;
+            }
+            else if (sqr > num)
+             right = mid - 1;
+            else
+             left = mid + 1;
+        }
+        return res;
+    }
+}
