da2f6640557e30bbfb33078083769d9029a00efb
Pushpak Bhagat <70573212+pushpak-256@users.noreply.github.com>
2022-05-21 21:14:39 +0530

    Time: 6 ms (27.32%), Space: 44.6 MB (9.02%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
@@ -1,30 +1,22 @@
 class Solution {
 	public int[] intersect(int[] a, int[] b) {
-  Map<Integer,Integer>map = new HashMap<>();
-  List <Integer>ll = new ArrayList<>(); 
-    for(int i :a)
-    {
-      if(map.containsKey(i))
-      {
-        map.put(i,map.get(i)+1);
-      }
-      else map.put(i,1);
-    }
-    
-     for(int i :b)
-    {
-      if(map.containsKey(i) && map.get(i)>0)
-      {
-        map.put(i,map.get(i)-1);
-         ll.add(i);
-      }
-    }
-     int res [] = new int [ll.size()];
-     int index=0;
-    for(int i:ll)
-    {
-      res[index++]=i;
-    }
-    return res;
-}
+		Map <Integer, Integer> map  = new HashMap<Integer, Integer>();
+		List<Integer>ll = new LinkedList<Integer>();
+		for (int x : a) {
+			if (map.containsKey(x)) {map.put(x,map.get(x)+1);}
+			else map.put(x, 1);
+		}
+
+		for (int x : b) {
+			if (map.containsKey(x) && map.get(x)>0)
+			{
+				map.put(x,map.get(x)-1);
+				ll.add(x);
+			}
+		}
+		int []res = new int [ll.size()];
+		int i =0;
+        for(int x : ll) {res[i++]=x;}		
+		return res;
+	}
 }
