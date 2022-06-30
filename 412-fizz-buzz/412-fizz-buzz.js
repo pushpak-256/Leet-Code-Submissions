b807ff41148d46ce194e6f37f21e83d538147eb2
Pushpak Bhagat <70573212+pushpak-256@users.noreply.github.com>
2022-06-30 18:22:26 +0530

    Time: 93 ms (57.28%), Space: 44.2 MB (80.78%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.js
+++ 412-fizz-buzz/412-fizz-buzz.js
@@ -5,21 +5,18 @@
 var fizzBuzz = function(n) {
 
    let i =1;
-  const res = []; 
-  
-  while(i<=n)
-  {
-    var divBy3 = false;
-    var divBy5 = false;
-    
-    if(i%3===0) divBy3 = true;
-    if(i%5===0) divBy5 = true;
-
-    if(divBy3 && divBy5) res.push("FizzBuzz");
-    else if(divBy3) res.push("Fizz");
-    else if(divBy5) res.push("Buzz");
-    else res.push(i.toString());
-    i++;
-  }
- return res;
+   const res = []; 
+   
+   while(i<=n)
+   {
+     var divBy3 = false;
+     var divBy5 = false;
+     
+     if(i%15===0) res.push("FizzBuzz");
+     else if (i%3===0) res.push("Fizz");
+     else if (i%5===0) res.push("Buzz");
+     else res.push(i.toString());
+     i++;
+   }
+  return res;
 };
