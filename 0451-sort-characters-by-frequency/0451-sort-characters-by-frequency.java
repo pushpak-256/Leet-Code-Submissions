ed62673601398c318d8b3724b1b133887dbc3818
Pushpak Bhagat <70573212+pushpak-256@users.noreply.github.com>
2024-02-07 13:13:54 +0530

    Time: 116 ms (18.58%), Space: 45.9 MB (24.30%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,22 +1,28 @@
 class Solution {
     public String frequencySort(String s) {
-        StringBuilder sb = new StringBuilder();
-        Map<Character, Integer> map = new HashMap<>();
+        Map <Character,Integer> map = new HashMap<>();
         
         for(char c : s.toCharArray())
-          map.put(c,map.getOrDefault(c,0)+1);
+            map.put(c,map.getOrDefault(c,0)+1);
         
-        Comparator <Character> compare = (a,b)->map.get(b)-map.get(a);
-        PriorityQueue <Character> pq = new PriorityQueue<>(compare);
-        pq.addAll(map.keySet());
+        Comparator <Character> compare = (b,a)->{
+            int diff = map.get(a)-map.get(b);
+            if(diff==0)
+                return a-b;
+            return diff;
+        };
         
-        while(!pq.isEmpty()){
-            char x = pq.poll();
-            int count = map.get(x);
-            while(count-->0)
-               sb.append(x);
-        }
-
+        PriorityQueue<Character> pq = new PriorityQueue<>(compare);
+        
+        for(char c : s.toCharArray())
+            pq.add(c);
+        
+        StringBuilder sb = new StringBuilder();
+        
+        while(!pq.isEmpty())
+            sb.append(pq.poll());
+        
         return sb.toString();
+        
     }
 }
